494ff6b66e6e8a0c69c676f0c9a05f656cc0c2c4
Shaurya-Ps-Bisht <shauryawbisht@gmail.com>
2024-07-29 16:14:06 +0530

    Exp section | Config Update for production/dev build base directories

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,9 +5,50 @@ import {
 
 import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
+import { styles } from "./styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    date={experience.date}
+    iconStyle={{ background: experience.iconBg }}
+    icon={
+      <div className="flex justify-center items-center w-full h-full">
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 classname="text-white text-[24px] font-bold">{experience.title}</h3>
+    </div>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>How I was not unemployed before</p>
+        <h2 className={styles.sectionHeadText}> XP </h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
 };
 
-export default Experience;
+export default SectionWrapper(Experience, "work");
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/portfolio-react-threejs",
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  return {
+    base: isProduction ? "/portfolio-a/" : "/",
+    plugins: [react()],
+  };
 });
